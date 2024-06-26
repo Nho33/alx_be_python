08a13e3c6cb7c6da0276cf469c5c8f6d8a4f27a1
Nho33 <nonophast@gmail.com>
2024-06-26 15:52:09 -0700

    Added arithmetic algorythm to the file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,3 +1,13 @@
 number1 = 10
 number2 = 5
+message = "Addition of 10 and 5 is"
+result = 10 + 5
+print((message) , result)
+message_2 = "Subtraction of 10 and 5 is"
+result_2 = 10 - 5
+print((message_2) , result_2)
+message_3 = "Multiplication of 10 and 5 is"
+result_3 = 10 * 5
+print((message_3) , result_3)
+
 
