75e50111776eac23b90af7469c73342e38b9f0ef
Nho33 <nonophast@gmail.com>
2024-06-28 13:14:53 -0700

    created file and assigned variables

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+income = int(input("Enter your monthly income: "))
+expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = income - expenses
+print("your monthly savings are" , monthly_savings)
+
+
