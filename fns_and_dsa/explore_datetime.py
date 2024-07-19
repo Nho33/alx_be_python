4a2ee68ca3c9047c1b2423df8f8ea6e88aa51a14
Nonopha <nonophast@gmail.com>
2024-07-19 13:30:00 -0700

    date and time script

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,24 +1,19 @@
 from datetime import datetime, timedelta
 
 def display_current_datetime():
+    global current_date
     current_date = datetime.now()
-    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Current date and time: {formatted_date}")
+    formated_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formated_date}")
 
-def calculate_future_date(days_to_add):
-    current_date = datetime.now()
-    future_date = current_date + timedelta(days=days_to_add)
-    firmatted_future_date = future_date.strftime("%Y-%m-%d")
-    print(f"Future date: {formatted_future_date}")
 
-def main():
-    display_current_datetime()
+number_of_days = int((input("Enter the number of days to add to the current date: ")))
 
-    try:
-        days_to_add = int(input("Enter the number of days to add to the current date: "))
-        calculate_future_date(days_to_add)
-    expect ValueError:
-        print("Invalid input. Please enter an integer value for the number of days.")
+def calculate_future_date():
+    future_date = current_date + timedelta(number_of_days)
+    formated_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Future date: {formated_date}") 
 
-    if __name__ == "__main__":
-        main()
+
+display_current_datetime()
+calculate_future_date()
