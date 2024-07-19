252d6bdf2bc21a9dd7368f769f0b89c1b5d1d367
Nonopha <nonophast@gmail.com>
2024-07-19 11:31:29 -0700

    tester

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -1,5 +1,7 @@
 import sys
-from main():
+from bank_account import BankAccount
+
+def main():
     account = BankAccount(100)
     if len(sys.argv) < 2:
         print("Usage: python main.py <command>:<amount>")
@@ -7,7 +9,7 @@ from main():
         sys.exit(1)
 
     command, *params = sys.argv[1].split(':')
-    amount = float(params[0]) if params else None:
+    amount = float(params[0]) if params else None
 
     if command == "deposit" and amount is not None:
         account.deposit(amount)
