4b2d42a25ec4577c087ab69a1fe3ad812457d672
Nonopha <nonophast@gmail.com>
2024-07-19 12:14:57 -0700

    fixing typos

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,11 +3,11 @@ def safe_divide(numerator, denominator):
     
         num = float( numerator)
         denom = float( denominator)
-        result = numerator / denominator
+        result = num / denom
         return f"Result: {result}"
 
     except ZeroDivisionError:
-        return "Error: Divisionby zero is not allowed."
+        return "Error: Division by zero is not allowed."
 
     except ValueError:
         return "Error: Non-numeric input provided."
