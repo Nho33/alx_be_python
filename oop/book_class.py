f28c81b35788d5ae35484b48c9192a5190cf68e3
Nonopha <nonophast@gmail.com>
2024-07-21 13:48:33 -0700

    Corrected the name of the book

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,14 +4,17 @@ class Book:
         self.author = author
         self.year = year
 
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
     def __str__(self):
         return f"Title = {self.title} by Author = {self.author},published in Year = {self.year}."
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
-    def __del__(self):
-        print(f"Deleting {self.title}.")
+#    def __del__(self):
+#        print(f"Deleting {self.title}.")
 
 b = Book("1984", "George Orwell", 1949)
 
