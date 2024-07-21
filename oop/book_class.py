2e05af3c52bb5984e1400bb53140d6986ae857c8
Nonopha <nonophast@gmail.com>
2024-07-21 13:37:03 -0700

    Created the book script

---
+++ oop/book_class.py
@@ -0,0 +1,36 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"Title = {self.title} by Author = {self.author},published in Year = {self.year}."
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    def __del__(self):
+        print(f"Deleting {self.title}.")
+
+b = Book("My sweet Audrina", "VC Andrews", 1982)
+
+# Import the Book class from book.py
+from book import Book
+
+def main():
+    # Create an instance of the Book class
+    my_book = Book("My Sweet Audrina", "VC Andrews", 300)
+
+    # Test the __str__ method
+    print("Testing __str__ method:")
+    print(my_book)
+
+    # Test the __repr__ method
+    print("\nTesting __repr__ method:")
+    print(repr(my_book))
+
+if __name__ == "__main__":
+    main()
+
+
