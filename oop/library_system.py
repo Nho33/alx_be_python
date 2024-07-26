592b6ad627eae6bb958a88d2f0295c0bfb809871
Nonopha <nonophast@gmail.com>
2024-07-26 13:28:49 -0700

    correcting spacing

---
+++ oop/library_system.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        self.file_sie = file_size
+            return int"{self.file_size}"
+
+class PrintBook(Book):
+    def__init__(self, title, author, page_count):
+        self.page_count = page_count
+            return int"{self.page_count}"
